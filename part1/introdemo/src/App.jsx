56befe1aa6533dc330dcf17ded5c4af11f4d3c6d
mariolopezDev <marioalbertolopez@uned.cr>
2025-04-20 14:19:15 -0600

    feat: Add initial introdemo project files

---
+++ part1/introdemo/src/App.jsx
@@ -0,0 +1,12 @@
+const App = () => {
+  console.log('Hello from component')
+  return (
+    <div>
+      <p>Hello world</p>
+    </div>
+  )
+}
+
+export default App
+// This is a simple React component that renders a div 
+// containing the text "Hello world".
